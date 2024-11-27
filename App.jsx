a9afa4264b9a13d501d8ecf78ba6c281a1aa325d
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-27 12:18:04 +0100

    import context in App.jsx

--- App.jsx
+++ App.jsx
@@ -24,27 +24,34 @@ import PostPage from './src/Pages/PostPage'
 //importo NotFound
 import NotFound from './src/Pages/NotFound'
 
+//importo PostContex
+import PostContex from './src/contexts/PostContex'
+
 function App() {
 
   return (
     <>
 
-      {/*uso BrowserRouter come container delle rotte*/}
-      <BrowserRouter>
-        {/*uso Routes per inserire le route singole e poterle gestire singolarmente*/}
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path='/' element={<Homepage />}></Route>
-            <Route path='/chisiamo' element={<ChiSiamo />}></Route>
-            <Route path='/listapost' element={<ListaPost />}></Route>
-            <Route path='/listapost/create' element={<CreaPost />}></Route>
-            <Route path='/listapost/:slug' element={<PostPage />}></Route>
-            <Route path='*' element={<NotFound />}></Route>
-          </Route>
+      <PostContex.Provider>
+        {/*uso BrowserRouter come container delle rotte*/}
+        <BrowserRouter>
+          {/*uso Routes per inserire le route singole e poterle gestire singolarmente*/}
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path='/' element={<Homepage />}></Route>
+              <Route path='/chisiamo' element={<ChiSiamo />}></Route>
+              <Route path='/listapost' element={<ListaPost />}></Route>
+              <Route path='/listapost/create' element={<CreaPost />}></Route>
+              <Route path='/listapost/:slug' element={<PostPage />}></Route>
+              <Route path='*' element={<NotFound />}></Route>
+            </Route>
 
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </PostContex.Provider>
+
+
     </>
   )
 }

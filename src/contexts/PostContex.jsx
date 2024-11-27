bea808499dbce97d8940fabd875b7dc601cb4092
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-27 12:07:20 +0100

    create contexts folder and create PostContext.jsx file in it

---
+++ src/contexts/PostContex.jsx
@@ -0,0 +1,8 @@
+//importo createContext da react
+import { createContext } from "react";
+
+//creo una costante per il PostContex
+const PostContex = createContext()
+
+//esporto PostContex
+export default PostContex
